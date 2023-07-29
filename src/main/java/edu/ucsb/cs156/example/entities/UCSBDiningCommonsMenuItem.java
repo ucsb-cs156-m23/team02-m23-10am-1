3bb2cf24dcfea347dd3df878de8a2a57347d5d3c
Hao Wu <hao_wu@csil.cs.ucsb.edu>
2023-07-29 15:59:03 -0700

    hw - fix MenuItem entity and repository

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
@@ -2,6 +2,8 @@ package edu.ucsb.cs156.example.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,8 @@ import lombok.Builder;
 @Entity(name = "ucsbdiningcommonsmenuitem")
 public class UCSBDiningCommonsMenuItem {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long Id;
   private String diningCommonsCode;
   private String name;
   private String station;
